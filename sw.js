4b6fdd88d83dafa9e7ce6f9361f64dc09f314cbc
kwris <kkieszek2@gmail.com>
2019-04-23 18:12:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,31 +11,4 @@ if ('serviceWorker' in navigator) {
   });
 }
 
-//caches
-const cacheName = 'v1';
-
-const cacheAssets = [
-'HHtribute.html'
-];
-
-//call install event
-self.addEventListener('install', e => {
-	console.log('Service Worker: Installed');
-	e.waitUntil(
-	caches
-	.open(cacheName)
-	.then(cache => {
-		console.log('Service Worker: Caching Files')
-		cache.addAll(cacheAssets);
-	})
-	.then(() => self.skipWaiting())
-	);
-});
-
-//call activate event
-self.addEventListener('activate', e => {
-	console.log('Service Worker: Activated');
-	//remove unwanted caches
-	
-});
 
