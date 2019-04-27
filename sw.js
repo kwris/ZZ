2f0ce83f6b5e7991723dd4c86f5ec1e940b4e8b9
kwris <kkieszek2@gmail.com>
2019-04-27 11:41:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,14 +11,4 @@ if ('serviceWorker' in navigator) {
   });
 }
 
- //call install event
-self.addEventListener('install', e => {
-	console.log('Service Worker: Installed');
-  });
 
-//call activate event
-self.addEventListener('activate', e => {
-	console.log('Service Worker: Activated');
-	//remove unwanted caches
-	
-}); 
