177a9019c0ca1c50eee5e301c860f241004f02ae
kwris <kkieszek2@gmail.com>
2019-04-27 13:44:18 +0200

    Update cache.js

--- cache.js
+++ cache.js
@@ -1,11 +1,32 @@
- //call install event
+const CacheName = 'v1';
+
+const cacheAssets = [
+'index.html',
+'sw.js',
+'cache.js',
+'ldg.png',
+'her1.jpg',
+'bm.jpg',
+'HH2013.jpg'	
+];
+
+//call install event
 self.addEventListener('install', e => {
 	console.log('Service Worker: Installed');
+	e.waitUntil(
+	caches
+	.open(cacheName)
+	.then(cache => {
+		console.log('Service Worker: Caching Files')
+		cache.addAll(cacheAssets);
+	})
+	.then(() => self.skipWaiting())
+	);
   });
 
-/*//call activate event
+//call activate event
 self.addEventListener('activate', e => {
 	console.log('Service Worker: Activated');
 	//remove unwanted caches
 	
-}); */
+}); 
