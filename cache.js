4036d2d1cb3fce4fc3f22625eced7549151edfa2
kwris <kkieszek2@gmail.com>
2019-04-27 11:43:05 +0200

    Update cache.js

--- cache.js
+++ cache.js
@@ -1,1 +1,11 @@
+ //call install event
+self.addEventListener('install', e => {
+	console.log('Service Worker: Installed');
+  });
 
+//call activate event
+self.addEventListener('activate', e => {
+	console.log('Service Worker: Activated');
+	//remove unwanted caches
+	
+}); 
